d26ce2b2491d54bfb141ca825fc2c70e8f3d7ab7
Draconic-saint <jkeirsmaekers@gmail.com>
2017-08-22 23:30:01 +0200

    prepared the model files for use

---
+++ models/users.js
@@ -0,0 +1,22 @@
+//hergeruikd het usermodel van examenopdracht 1e zit, minimaal aagepast//
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Account = new Schema({
+    name:String,
+    profilepic:String,
+    //courses: Array,
+    role: String,
+    //demo:Number,
+    //die:Number,
+    //vote:Boolean,
+    //onStage:Boolean,
+    //date:String
+    discussions:Number,
+    comments:Number
+} ,{
+    collection: 'DemoOrDie'
+});
+
+module.exports = mongoose.model('Account', Account);
