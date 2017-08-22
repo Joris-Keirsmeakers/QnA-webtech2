a3c34bcf1445e346b02dcc702fc2b70d8d28e623
Draconic-saint <jkeirsmaekers@gmail.com>
2017-08-22 22:50:19 +0200

    database set-up

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+var config = require('./config/config.js');
+mongoose.connect(config.database);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('gutten tag World!');
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+  console.log('App listening on port 3000!')
 });
---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {
+  database: 'mongodb://localhost/',
+  port: 3000
+};
+
+module.exports = config;
