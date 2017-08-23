24c151a50990dd631257ddeb7eb2a807bd0e77a8
Draconic-saint <jkeirsmaekers@gmail.com>
2017-08-23 23:48:55 +0200

    Added some forgotten bits to the discussions model

--- models/discussions.js
+++ models/discussions.js
@@ -3,6 +3,25 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var discussion = new schema({
-  creators
+  location:String,
+  timestamp:String,
+  creator:{
+    username: String,
+    avatar:String
+  },
+  question: String,
+  comments:{
+    comment:{
+      user:{
+        userame:String,
+        avatar:String
+      }
+      text:String
+    }
+  }
 
-})
+},{
+    collection: 'QnA-discussions'
+});
+
+module.exports = mongoose.model('Account', Account);
